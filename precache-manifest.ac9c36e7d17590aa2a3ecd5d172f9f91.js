578168fe7465cb316080b7ecd69c9b7a6633b645
SpeedMyLife <lishchenko.d@gmail.com>
2019-08-02 22:06:16 +0300

    Updates

---
+++ precache-manifest.ac9c36e7d17590aa2a3ecd5d172f9f91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199f56f7760a91aa4b2de236ec50943a",
+    "url": "/labyrinth/index.html"
+  },
+  {
+    "revision": "93f9f5dd97205b119389",
+    "url": "/labyrinth/static/css/main.a717c26b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/labyrinth/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "93f9f5dd97205b119389",
+    "url": "/labyrinth/static/js/main.da5239a3.chunk.js"
+  },
+  {
+    "revision": "17e89474fa7acda5f1e6",
+    "url": "/labyrinth/static/js/runtime~main.ab342c95.js"
+  },
+  {
+    "revision": "db526c43088bd415f7ec8aa71d530cbe",
+    "url": "/labyrinth/static/media/arrow.db526c43.png"
+  },
+  {
+    "revision": "2f5d3f9a71f5c99e3bcf0a156c42a868",
+    "url": "/labyrinth/static/media/correct.2f5d3f9a.png"
+  },
+  {
+    "revision": "872d0c8e65673a24bccf637f04fb1957",
+    "url": "/labyrinth/static/media/wrong.872d0c8e.png"
+  }
+]);
